ee44579c9526673ae73dec469e1e9f7b3f87ffb9
amin <77453359+aminiscoding@users.noreply.github.com>
2023-10-10 12:28:43 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, LLMPredictor, ServiceContext, load_index_from_storage, StorageContext
+from langchain.llms import OpenAI
+import gradio as gr
+import os
+
+os.environ["OPENAI_API_KEY"] = 'API_Key'
+
+def construct_index(directory_path):
+    num_outputs = 512
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+    docs = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTVectorStoreIndex.from_documents(docs, service_context=service_context)
+
+    index.storage_context.persist(persist_dir='index.json')
+
+    return index
+
+def chatbot(input_text):
+    query_engine = index.as_query_engine()
+    response = query_engine.query(input_text)
+    return response.response
+
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.Textbox(lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+index = construct_index("docs")
+iface.launch(share=True)
